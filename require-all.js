61a60cabf0151029bce2fc288904a82976b1f976
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-08-20 17:22:56 +0430

    feat(module): add a modifier and also get filename as paramater to igonre the file that has been invoked from there

--- require-all.js
+++ require-all.js
@@ -1,23 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-const camelCase = name =>
+const camelCase = (name, splitChar = '-') =>
   name
-    .substr(0,
-      name.lastIndexOf('.') > 0 ?
-        name.lastIndexOf('.') :
-        name.length)
-    .split('-')
+    .split(splitChar)
     .map((word, index) =>
-      index ?
+      (index ?
         word[0].toUpperCase() + word.slice(1) :
-        word)
+        word))
     .join('');
 
-// Requires all the modules in `dirname` path and
-// exports them with camelCase style of their filename
-module.exports = dirname =>
-  fs.readdirSync(dirname).filter(fileName => fileName.toLowerCase() !== 'index.js').reduce((exports, requireName) =>
-    (exports[camelCase(requireName)] = require(path.join(dirname, requireName)),
-      exports), {});
+const removeFileExt = name =>
+  name.substr(0,
+    name.lastIndexOf('.') > 0 ?
+      name.lastIndexOf('.') :
+      name.length
+  );
 
+// Requires all the modules from the path of `modulePath` and
+// exports them with camelCase style of their filename
+// also runs a modifier before exporting them as an object
+const requireAll = (modulePath, modifier = m => m) => {
+  const [dirname, basename] = [
+    path.dirname(modulePath),
+    path.basename(modulePath),
+  ];
+  return fs.readdirSync(dirname)
+    .filter(fileName => fileName.toLowerCase() !== basename)
+    .map(modifier)
+    .reduce((exports, requireName) =>
+      (exports[camelCase(removeFileExt(requireName))] = require(path.join(dirname, requireName)),
+      exports),
+    {});
+};
+
+module.exports = requireAll;
